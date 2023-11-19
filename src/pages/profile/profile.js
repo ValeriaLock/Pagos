beae0f33c463a3db6ca55bbca42353de71854aeb
ValeriaLock <valeria.lock@utec.edu.pe>
2023-11-19 18:31:37 -0500

    a

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -3,6 +3,9 @@ import './profile.scss';
 import Form from 'devextreme-react/form';
 
 export default function Profile() {
+  const handlePaymentToken = (token) => {
+    console.log(token);
+  };
   const [notes, setNotes] = useState(
     'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if you`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.'
   );
@@ -35,6 +38,14 @@ export default function Profile() {
         <span>{notes}</span>
       </div>
 
+      <div>
+        <stripe-buy-button
+          buy-button-id="buy_btn_1OEK0GAFFbvLfn23siZgFODV"
+          publishable-key="pk_test_51OEJi2AFFbvLfn239X7UMMdeCaTFWdSlRKgyL1DEebJ5e77TrGhfjoZvqjkbggtXrMETXGoOcE6PyQVihW4yDH1w00ycCaynVT"
+        >
+        </stripe-buy-button>
+      </div>
+      
       <div className={'content-block dx-card responsive-paddings'}>
         <Form
           id={'form'}
